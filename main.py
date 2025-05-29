637c66e5b50ad13bc8387d6194ad9a6fc12fd621
Юлия (Джулия) <sixquestions666@gmail.com>
2025-05-29 16:32:17 +0300

    Correct code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def показать_дату():
     from datetime import datetime
     сейчас = datetime.now()
+
     print(f"Сегодня: {сейчас.strftime('%A, %d %B %Y')}")
     print(f"Время: {сейчас.strftime('%H:%M:%S')}")
+# Вызов второй функции
+(показать_дату())
 
-показать_дату()
